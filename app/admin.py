903fcbfcef8b9372e515d9f1814240f21a25eab9
sharmaupasana <sharmaupasana493@gmail.com>
2025-03-17 13:47:47 +0545

    DB Part completed

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from app.models import Book, Course, Department, Semester, Student
 
 # Register your models here.
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    
+
+@admin.register(Semester)
+class SemesterAdmin(admin.ModelAdmin):
+    list_display = ['name', 'symbol']
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'department']
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code', 'course', 'semester', 'edition']
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'symbol', 'department', 'course', 'semester']
+    
--- app/models.py
+++ app/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+
+
+class Semester(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    symbol = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Course(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True)
+    semesters = models.ManyToManyField(Semester)
+    
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=255, blank=True, null=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE, null=True)
+    edition = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name + " - " + self.edition
+
+
+class Student(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    symbol = models.CharField(max_length=255, unique=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True)
+    image = models.ImageField(upload_to='students/', blank=True, null=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE, null=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class StudentBook(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, null=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True)
+    issue_date = models.DateField()
+    return_date = models.DateField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.student.name + " - " + self.book.name
